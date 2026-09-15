389b9d0fc9638ded9910d1eac41d34eabd7c8de8
Pablo Dauga <pablodauga@gmail.com>
2026-09-15 13:43:13 +0200

    leaderboard rescaled

--- src/drawing/pacman_drawer.py
+++ src/drawing/pacman_drawer.py
@@ -38,7 +38,7 @@ class PacManDrawer(MazeDrawer):
         x1, y1 = px, py
         x2, y2 = px + self.cell_size, py + self.cell_size
 
-        gum = (6, (255, 255, 210)) if not super else (12, (255, 60, 180))
+        gum = (5, (255, 255, 210)) if not super else (7, (255, 60, 180))
         xc, yc = (x1 + x2) // 2, (y1 + y2) // 2
 
         self.draw_rect(
--- src/main_menu.py
+++ src/main_menu.py
@@ -11,7 +11,8 @@ class Menu:
 
         # leaderboard
         self.scorer = Scorer("score.json")
-        self.score = self.scorer._load()
+        score = self.scorer._load()
+        self.score = dict(sorted(score.items(), key=lambda item: item[1], reverse=True))
         self.scorers_rendered: list[tuple[int, pygame.Surface]] = []
 
         # init render
@@ -23,30 +24,56 @@ class Menu:
         self.print_leaderboard()
 
     def render_leaderboard(self) -> None:
-        self.scorers_rendered = []
+        w, h = self.frame.size
 
+        self.scorers_rendered = []
+        self.font_name = pygame.font.Font(None, w // 25)
+
+        self.max_width_user = 0
+        self.max_width_score = 0
+
+        for i, (key, value) in enumerate(self.score.items()):
+            if i >= 10:
+                return None # a tester
+
+            if len(key) > 15:
+                key = key[:15] + "..."
+
+            user = self.font_name.render(f"{i + 1}: {key}   ",
+                True, (255, 255, 255))
+
+            score = self.font_name.render(f"{value}",
+                True, (255, 255, 255))
+
+            self.max_width_user = max(self.max_width_user, user.get_width())
+            self.max_width_score = max(self.max_width_score, score.get_width())
+
+            self.scorers_rendered.append(
+                (
+                    value,
+                    user,
+                    score
+                )
+            )
+
+    def print_leaderboard(self) -> None:
         w, h = self.frame.size
         self.frame.draw_rect((0, 0), (w // 2, h), (50, 75, 100))
 
-        self.font_name = pygame.font.Font(None, h // 10)
-        for i, (key, value) in enumerate(self.score.items()):
-            if i >= 10:
-                return None
-            self.scorers_rendered.append(
-                (value, self.font_name.render(
-                    f"{i + 1}: {key} - " + f"{value}",
-                    True, (255, 255, 255)))
-            )
+        px, py = self.frame.size
+        ox, oy = (
+            px // 4 - self.max_width_user // 2 - self.max_width_score // 2,
+            py // 10
+        )
 
-        self.scorers_rendered = sorted(self.scorers_rendered, reverse=True)
-
-    def print_leaderboard(self) -> None:
-        h = 0
-        for i in range(len(self.scorers_rendered)):
-            score, scorer = self.scorers_rendered[i]
-            hv = scorer.get_height()
+        for n, user, score in self.scorers_rendered:
             self.frame.surface.blit(
-                scorer,
-                (0, h)
+                user, (ox, oy)
             )
-            h += hv
+            self.frame.surface.blit(
+                score, (ox + self.max_width_user, oy)
+            )
+            oy += max(
+                user.get_height(), score.get_height()
+            )
+
